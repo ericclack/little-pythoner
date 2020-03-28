d4958f47ff9e5843c496a0a0a2dc312fb80d89a2
Eric Clack <eric@bn7.net>
2020-03-28 21:00:41 +0000

    Github link

--- doc/conf.py
+++ doc/conf.py
@@ -80,7 +80,14 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'show_powered_by': False,
+    'github_user': 'ericclack',
+    'github_repo': 'little-pythoner',
+    'github_banner': True,
+    'show_related': False,
+    #'note_bg': '#FFF59C'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
